9a28e58cfeed4c4d742c83a3d0280d51132ff2a5
Pouiiit <skyrunner106@gmail.com>
2021-02-03 19:03:46 +0100

    Navbar responsive V1

--- components/navbar.js
+++ components/navbar.js
@@ -1,18 +1,32 @@
 import style from "../styles/navbar.module.css";
 import Image from "next/image";
 
-export default () => {
+const Navbar = () => {
     return(
-        <nav className={style.container}>
-            <div className={style.logoContainer}>
-                <Image src="/logo-navbar.svg" alt="Logo Portfolio" width="80px" height="41px"/>
-                <a className={style.link}>Robin Minster</a>
-            </div>
-            <a className={style.link}>À propos</a>
-            <a className={style.link}>Compétences</a>
-            <a className={style.link}>Expériences</a>
-            <a className={style.link}>Contact</a>
-            <a className={style.link}>EN | FR</a>
-        </nav>
+        <header className={style.box}>
+            <nav className={style.container}>
+                <div className={style.c_nav_desktop}>
+                    <div className={style.c_logo}>
+                        <div><Image src="/logo-navbar.svg" alt="Logo Portfolio" width="80px" height="41px"/></div>
+                        <a className={style.name}>Robin Minster</a>
+                    </div>
+                    <a className={style.link}>À propos</a>
+                    <a className={style.link}>Compétences</a>
+                    <a className={style.link}>Expériences</a>
+                    <a className={style.link}>Contact</a>
+                    <a className={style.language}>EN | FR</a>
+                </div>
+
+                <div className={style.c_nav_mobile}>
+                    <a className={style.name}>Robin Minster</a>
+                    <div><Image src="/logo-navbar.svg" alt="Logo Portfolio" width="45px" height="35px"/></div>
+                    <div className={style.hamburger_menu}>
+                        <Image src="/menu.svg" alt="Hamburger Menu" width="20px" height="20px"/>
+                    </div>
+                </div>
+            </nav>
+        </header>
     );
-}
+};
+
+export default Navbar;
