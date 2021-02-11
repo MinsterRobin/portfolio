2795d597717dc841f377bbb19c347b13a2543ab5
Pouiiit <skyrunner106@gmail.com>
2021-02-11 15:03:37 +0100

    Clean About Section with some tweaks.

--- components/separator.js
+++ components/separator.js
@@ -3,9 +3,11 @@ import Image from "next/image"
 
 const Separator = () => {
   return(
-      <div className={style.c_separator}>
+/*      <div className={style.c_separator}>
           <Image src={"/Separator.svg"} alt="Separator" width="600px" height="30px"/>
-      </div>
+      </div>*/
+
+    <hr className={style.hr}/>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css';
 import data from '../components/data.json';
 import Navbar from '../components/navbar';
 import About from "../components/about";
-/*import Separator from "../components/separator";*/
+import Separator from "../components/separator";
 
 export default function Home() {
   return (
@@ -12,9 +12,9 @@ export default function Home() {
         <title>{data.title}</title>
       </header>
       <body className={styles.mainContainer}>
-        <Navbar/>
-        <About/>
-{/*        <Separator/>*/}
+        <Navbar />
+        <About />
+        <Separator />
       </body>
     </div>
   )
