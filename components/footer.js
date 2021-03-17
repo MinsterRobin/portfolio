486bcf5ff29b31179ac955b1eccb1b48aa6c050b
Pouiiit <skyrunner106@gmail.com>
2021-03-17 15:18:47 +0100

    - footer.js = Animation de l'oeil avec React-Cursor-position.

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,6 @@
-import style from "../styles/Footer.module.css";
 import styled from "styled-components";
 import React, {useState} from "react";
 import ReactCursorPosition from "react-cursor-position";
-
 
 const Footer_SC = styled.div`
     margin-top: 150px;
@@ -55,10 +53,10 @@ const Footer_SC = styled.div`
         
     .c_eye {
         position: absolute;
-        top: 40px;
+        top: 35px;
         left: 90px;
         width: 125px;
-        height: 63px;
+        height: 70px;
     }
     
     .social_logo {
@@ -81,7 +79,7 @@ const Footer_SC = styled.div`
 const Eye = styled.div`
     position: absolute;
     transform: translateX(${props => props.eyePos.x}) translateY(${props => props.eyePos.y});
-    transition: 50ms ease-in-out;
+    transition: 70ms ease-in-out;
 `;
 
 const mapRange = (amount, fromMin, fromMax, toMin, toMax) => {
@@ -100,19 +98,13 @@ const Footer = () => {
 };
 
 const CursorPositionGetter = (props) => {
-    const [eyePos, setEyePos] = useState({x: "50px", y: "7px"});
+    const [eyePos, setEyePos] = useState({x: "50px", y: "13px"});
 
     const {
-        detectedEnvironment: {
-            isMouseDetected = false,
-            isTouchDetected = false
-        } = {},
         elementDimensions: {
             width = 0,
             height = 0
         } = {},
-        isActive = false,
-        isPositionOutside = false,
         position: {
             x = 0,
             y = 0
@@ -122,19 +114,14 @@ const CursorPositionGetter = (props) => {
     return (
         <div
             onMouseMove={(e) => {
-            setEyePos({
-                        x: mapRange(x, 0, window.innerWidth, 0, 85) + "px",
-                        y: mapRange(y, 0, height, 0, 20) + "px",
-                    }
-                );
-            }}
-            onMouseLeave={() => { setEyePos({x:"50px", y:"7px"})}}
+                setEyePos({
+                            x: mapRange(x, 0, window.innerWidth, 0, 85) + "px",
+                            y: mapRange(y, 0, height, 0, 28) + "px",
+                        }
+                    );
+                }}
+            onMouseLeave={() => { setEyePos({x:"50px", y:"13px"})}}
         >
-            x: {x}
-            {`y: {y}`}
-            {`width:: ${width}`}
-            {`height: ${height}`}
-            {`isActive: ${isActive}`}
             <div className="c_content">
                 <div className="c_logo">
                     <img className="logo" src={"/Footer-Logo.svg"} alt="Footer Logo" />
@@ -146,19 +133,19 @@ const CursorPositionGetter = (props) => {
                 </div>
 
                 <div className="c_socials">
-                    <div className="c_social" onMouseEnter={() => setEyePos({x: "120px", y: "60px"})} onMouseLeave={() => setEyePos({x: "90px", y: "50px"})}>
+                    <div className="c_social" >
                         <img className="social_ring" src={"Social_Ring.svg"} alt="Social Ring"/>
                         <img className="social_logo" src={"/Linkedin.svg"} alt={"Linkedin Logo"}/>
                     </div>
-                    <div className="c_social" onMouseEnter={() => setEyePos({x: "100px", y: "60px"})} onMouseLeave={() => setEyePos({x: "90px", y: "50px"})}>
+                    <div className="c_social" >
                         <img className="social_ring" src={"Social_Ring.svg"} alt="Social Ring"/>
                         <img className="social_logo" src={"/Mail.svg"} alt={"Mail Logo"}/>
                     </div>
-                    <div className="c_social" onMouseEnter={() => setEyePos({x: "80px", y: "60px"})} onMouseLeave={() => setEyePos({x: "90px", y: "50px"})}>
+                    <div className="c_social" >
                         <img className="social_ring" src={"Social_Ring.svg"} alt="Social Ring"/>
                         <img className="social_logo" src={"/Malt.svg"} alt={"Malt Logo"}/>
                     </div>
-                    <div className="c_social" onMouseEnter={() => setEyePos({x: "60px", y: "60px"})} onMouseLeave={() => setEyePos({x: "90px", y: "50px"})}>
+                    <div className="c_social" >
                         <img className="social_ring" src={"Social_Ring.svg"} alt="Social Ring"/>
                         <img className="social_logo" src={"/Github.svg"} alt={"Github Logo"}/>
                     </div>
