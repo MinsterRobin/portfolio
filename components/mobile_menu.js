33771fec7744bd385f00ce5d71a1b558b3fa35e9
MinsterRobin <minster.robin.pro@gmail.com>
2021-06-18 11:46:13 +0200

    + Transition spring sur le menu mobile. . Fix des liens mobiles qui ne fonctionnaient plus.

--- components/mobile_menu.js
+++ components/mobile_menu.js
@@ -4,14 +4,14 @@ import Language_Context from "../components/language-context";
 import data from "./data";
 import LanguageSelector from "./LanguageSelector";
 import Text_LanguageBased_OpacityTransition_SC from "./Text_LanguageBased_OpacityTransition_SC";
+import {animated, useSpring} from "react-spring";
 
-const Mobile_Menu_SC = styled.div`
+const Mobile_Menu_SC = animated(styled.div`
     position: absolute;    
     width: 100vw;
     height: calc(100vh - 70px);
     top: 70px;
     right: 0;
-    transform: translateX(${props => props.open ? "0vw" : "100vw"});
     
     background: linear-gradient(#0E0E0E 0%, #111111 3%);
     
@@ -22,8 +22,6 @@ const Mobile_Menu_SC = styled.div`
     gap: 30px;
     
     overflow: hidden;
-    
-    transition: transform ease-in-out 400ms;
     
     .c_mobile_links {
         margin: 0;
@@ -38,7 +36,7 @@ const Mobile_Menu_SC = styled.div`
     @media (min-width: 992px) {
         display: none;
     }
-`;
+`);
 
 const MobileLink_SC = styled(Text_LanguageBased_OpacityTransition_SC)`
     font-size: 20px;
@@ -53,33 +51,36 @@ const MobileLink_SC = styled(Text_LanguageBased_OpacityTransition_SC)`
 
 const Mobile_Menu = (props) => {
     const [languageContext] = useContext(Language_Context);
+    const springTransition = useSpring({
+        transform: props.open ? "translateX(0vw)" : "translate(100vw)",
+    });
 
     return(
-        <Mobile_Menu_SC open={props.open}>
+        <Mobile_Menu_SC style={springTransition} open={props.open}>
             <div className="c_mobile_links">
 
-                <MobileLink_SC
+                <MobileLink_SC as="a"
                     href="/#about"
                     className="mobile_link"
                     onClick={() => props.setOpen(!open)} >
                     {data[languageContext.currentLanguage].nav.about}
                 </MobileLink_SC>
 
-                <MobileLink_SC
+                <MobileLink_SC as="a"
                     href="/#skills"
                     className="mobile_link"
                     onClick={() => props.setOpen(!open)} >
                     {data[languageContext.currentLanguage].nav.skills}
                 </MobileLink_SC>
 
-                <MobileLink_SC
+                <MobileLink_SC as="a"
                     href="/#experiences"
                     className="mobile_link"
                     onClick={() => props.setOpen(!open)} >
                     {data[languageContext.currentLanguage].nav.exps}
                 </MobileLink_SC>
 
-                <MobileLink_SC
+                <MobileLink_SC as="a"
                     href="/#contact"
                     className="mobile_link"
                     onClick={() => props.setOpen(!open)} >
